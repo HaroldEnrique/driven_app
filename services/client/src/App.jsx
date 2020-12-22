779b944229f16346fc0ff069a0778806f07d9706
HaroldEnrique <haroldcotacallapa@upeu.edu.pe>
2020-12-21 23:37:04 -0500

    react auth

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -25,6 +25,8 @@ class App extends Component {
     };
     this.addUser = this.addUser.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleUserFormSubmit = this.handleUserFormSubmit.bind(this);
+    this.handleFormChange= this.handleFormChange.bind(this);
   };
   componentDidMount() {
     this.getUsers();
@@ -52,6 +54,31 @@ class App extends Component {
     obj[event.target.name] = event.target.value;
     this.setState(obj);
   };
+
+  handleUserFormSubmit(event) {
+    event.preventDefault();
+    const formType = window.location.href.split('/').reverse()[0];
+    let data = {
+      email: this.state.formData.email,
+      password: this.state.formData.password,
+    };
+    if (formType === 'register') {
+      data.username = this.state.formData.username;
+    }
+    const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/${formType}`
+    axios.post(url, data)
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((err) => { console.log(err); });
+  };
+
+  handleFormChange(event) {
+    const obj = this.state.formData;
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  };
+
   render() {
     return (
     <div>
@@ -66,12 +93,16 @@ class App extends Component {
                       <Form
                         formType={'Register'}
                         formData={this.state.formData}
+                        handleUserFormSubmit={this.handleUserFormSubmit}  // nuevo
+                        handleFormChange={this.handleFormChange}
                       />
                     )} />
                     <Route exact path='/login' render={() => (
                       <Form
                         formType={'Login'}
                         formData={this.state.formData}
+                        handleUserFormSubmit={this.handleUserFormSubmit}  // nuevo
+                        handleFormChange={this.handleFormChange}
                       />
                     )} />
                   <Route exact path='/' render={() => (
